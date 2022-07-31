77bddd39c24689fd8cacf383697953a48c121c7d
Mahone1985 <91633542+Mahone1985@users.noreply.github.com>
2022-07-31 22:51:28 +0100

    added path module

---
+++ index.js
@@ -0,0 +1,6 @@
+const path = require('path');
+const filePath = "C:\\temp\\file.txt";
+
+console.log(path.dirname(filePath));
+console.log(path.basename(filePath));
+console.log(path.extname(filePath));
