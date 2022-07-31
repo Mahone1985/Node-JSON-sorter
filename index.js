571a0258dd0f68f9314d21db6d0ee6895c5d53e8
Mahone1985 <91633542+Mahone1985@users.noreply.github.com>
2022-07-31 23:47:12 +0100

    Added path module

--- index.js
+++ index.js
@@ -1,6 +1,56 @@
-const path = require('path');
-const filePath = "C:\\temp\\file.txt";
+// Module : Working with file path information
+// https://nodejs.dev/learn/nodejs-file-paths
 
-console.log(path.dirname(filePath));
-console.log(path.basename(filePath));
-console.log(path.extname(filePath));
+const path = require('path');
+const filePath = "C:\\temp\\test.json";
+
+//console.log(path.dirname(filePath));
+//console.log(path.basename(filePath));
+//console.log(path.extname(filePath));
+
+// Load contents of the file
+// https://nodejs.dev/learn/reading-files-with-nodejs
+
+const fs = require('fs');
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  //console.log(data);
+  sorter(data);
+});
+
+const content = "samuelljackson";
+
+fs.writeFile("C:\\temp\\sorted.txt", content, err => {
+  if (err) {
+    console.error(err);
+  }
+  // file written successfully
+  console.log("File Written successfully");
+});
+
+
+
+
+function sorter(fileContent) {
+const itemObj = JSON.parse(fileContent);
+
+itemObj.item.sort((a, b) => {
+    let fa = a.name.toLowerCase(),
+        fb = b.name.toLowerCase();
+
+    if (fa < fb) {
+        return -1;
+    }
+    if (fa > fb) {
+        return 1;
+    }
+    return 0;
+});
+
+//console.log(JSON.stringify(itemObj));
+
+}
