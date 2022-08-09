396b2b4e1dbaf32756d28e64aa6a5913b3d79151
w4dd325 <w4dd325@gmail.com>
2022-08-09 21:19:48 +0100

    Updated

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+displayTitle();
+
 // Module : Working with file path information
 // https://nodejs.dev/learn/nodejs-file-paths
 
@@ -10,36 +12,61 @@ const filePath = "C:\\temp\\test.json";
 const fs = require('fs');
 
 fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  sorter(data);
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log("");
+    console.log("Reading file: " + path.basename(filePath));
+    sorter(data);
 });
-
 
 function sorter(fileContent) {
-const itemObj = JSON.parse(fileContent);
+    const itemObj = JSON.parse(fileContent);
 
-itemObj.item.sort((a, b) => {
-    let fa = a.name.toLowerCase(),
-        fb = b.name.toLowerCase();
+    console.log("---------------------------------------------------------------------------");
+    console.log("Starting to sort...");
 
-    if (fa < fb) {
-        return -1;
-    }
-    if (fa > fb) {
-        return 1;
-    }
-    return 0;
-});
+    itemObj.item.sort((a, b) => {
+        let fa = a.name.toLowerCase(),
+            fb = b.name.toLowerCase();
 
-fs.writeFile(path.dirname(filePath)+"\\sorted.JSON", JSON.stringify(itemObj), err => {
-    if (err) {
-      console.error(err);
-    }
-    // file written successfully
-    console.log("File Written successfully");
-  });
+        if (fa < fb) {
+            return -1;
+        }
+        if (fa > fb) {
+            return 1;
+        }
+        return 0;
+    });
 
+    //EXAMPLE OUTPUTS FOR PATH PACKAGE...
+    //-----------------------------------
+    //console.log(path.dirname(filePath));
+    //console.log(path.basename(filePath));
+    //console.log(path.extname(filePath));
+    //console.log(path.basename(filePath, '.json'));
+    //console.log(path.dirname(filePath) + "\\" + path.basename(filePath, '.json') + "_sorted.json");
+
+    fs.writeFile(path.dirname(filePath) + "\\" + path.basename(filePath, '.json') + "_sorted.json", JSON.stringify(itemObj), err => {
+        if (err) {
+            console.error(err);
+        }
+        // file written successfully
+        console.log("---------------------------------------------------------------------------");
+        console.log("File Written successfully");
+        console.log("Path: " + path.dirname(filePath) + "\\" + path.basename(filePath, '.json') + "_sorted.json")
+    });
+
+}
+
+function displayTitle() {
+    console.log(" _____                            _          _____            _            ");
+    console.log("|  __ \\                          | |        / ____|          | |           ");
+    console.log("| |__) |___  __ _ _   _  ___  ___| |_ ___  | (___   ___  _ __| |_ ___ _ __ ");
+    console.log("|  _  // _ \\/ _` | | | |/ _ \\/ __| __/ __|  \\___ \\ / _ \\| '__| __/ _ \\ '__|");
+    console.log("| | \\ \\  __/ (_| | |_| |  __/\\__ \\ |_\\__ \\  ____) | (_) | |  | ||  __/ |   ");
+    console.log("|_|  \\_\\___|\\__, |\\__,_|\\___||___/\\__|___/ |_____/ \\___/|_|   \\__\\___|_|   ");
+    console.log("               | |                                                         ");
+    console.log("               |_|                                                         ");
 }
